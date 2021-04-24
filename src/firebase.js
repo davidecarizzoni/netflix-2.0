adb6675267113662ab4fe5f789418826b9cef837
davidecarizzoni <davidecarizzoni@gmail.com>
2021-04-24 23:21:11 +0200

    Init project (firebase tools not yet installed)

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD92m_1TedmFStfZoP-1i-PRsmw9uC17vs",
+    authDomain: "netflix-clone-31e18.firebaseapp.com",
+    projectId: "netflix-clone-31e18",
+    storageBucket: "netflix-clone-31e18.appspot.com",
+    messagingSenderId: "404555204545",
+    appId: "1:404555204545:web:f5e2b6e696478540869140"
+  };
