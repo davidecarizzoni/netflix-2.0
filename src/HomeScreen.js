8dfb1234344a8b3467e8c1b3979fc21539006f2b
davidecarizzoni <davidecarizzoni@gmail.com>
2021-04-25 22:24:06 +0200

    Rows works. Home page frontend finisched

--- src/HomeScreen.js
+++ src/HomeScreen.js
@@ -2,6 +2,8 @@ import React from "react";
 import Banner from "./Banner";
 import "./HomeScreen.css";
 import Nav from "./Nav";
+import requests from "./Requests";
+import Row from "./Row";
 
 function HomeScreen() {
     return (
@@ -10,7 +12,20 @@ function HomeScreen() {
 
             <Banner />
 
-            {/* Row */}
+            {/* Six rows. Each rows have different props */}
+            <Row
+                title="NETFLIX ORIGINAL"
+                fetchUrl={requests.fetchNetflixOriginals}
+                isLargeRow
+            /> 
+            <Row  title="Trending now" fetchUrl={requests.fetchTrending} /> 
+            <Row  title="Top Rated" fetchUrl={requests.fetchTopRated} /> 
+            <Row  title="Action movie" fetchUrl={requests.fetchActionMovie} /> 
+            <Row  title="Comedy movie" fetchUrl={requests.fetchComedyMovies} /> 
+            <Row  title="Horror movie" fetchUrl={requests.fetchHorrorMovie} /> 
+            <Row  title="Romance movie" fetchUrl={requests.fetchRomanceMovie} /> 
+            <Row  title="Documentaries" fetchUrl={requests.fetchDocumentaries} />            
+       
         </div>
     )
 }
