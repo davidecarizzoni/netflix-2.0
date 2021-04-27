94903ebf9254af2bd27ef2d7ca68907b86792c04
davidecarizzoni <davidecarizzoni@gmail.com>
2021-04-27 16:51:16 +0200

    auth works

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from "firebase";
+
 const firebaseConfig = {
     apiKey: "AIzaSyD92m_1TedmFStfZoP-1i-PRsmw9uC17vs",
     authDomain: "netflix-clone-31e18.firebaseapp.com",
@@ -5,4 +7,12 @@ const firebaseConfig = {
     storageBucket: "netflix-clone-31e18.appspot.com",
     messagingSenderId: "404555204545",
     appId: "1:404555204545:web:f5e2b6e696478540869140"
-  };
+};
+
+// Initialize firebase db and autentication
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { auth };
+export default db;
